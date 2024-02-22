e493401e768b3633079003dc2851024ba8d64b84
Warner Vance <warnervance@icloud.com>
2024-02-22 12:12:35 -0500

    Finished all the TODO's. Made it so the adjustments actually work
    Signed-off-by: Warner Vance <warnervance@icloud.com>

--- Lin_reg_test.py
+++ Lin_reg_test.py
@@ -5,28 +5,27 @@ import pandas as pd
 
 # Inputs the data from a csv file named data.
 # The csv must be 2 columns with column 0 being x and column y being 1
-# This  programs assumes that we change the x value to linearize the data and not the y value
 
 df = pd.read_csv("Data.csv")
 x_column = input("Enter the x column name: ")
 
 
-def calculate_r_squared(df):
-    x_mean = df.iloc[:, 0].mean()
-    y_mean = df.iloc[:, 1].mean()
+def calculate_r_squared(dataframe):
+    x_mean = dataframe.iloc[:, 0].mean()
+    y_mean = dataframe.iloc[:, 1].mean()
 
     # This calculates the S_xx and S_yy. I
     x_variance = []
     y_variance = []
-    for i in range(len(df)):
-        x_variance.append((df.iloc[i, 0] - x_mean) ** 2)
-    for i in range(len(df)):
-        y_variance.append((df.iloc[i, 1] - y_mean) ** 2)
+    for i in range(len(dataframe)):
+        x_variance.append((dataframe.iloc[i, 0] - x_mean) ** 2)
+    for i in range(len(dataframe)):
+        y_variance.append((dataframe.iloc[i, 1] - y_mean) ** 2)
 
-    # Calculate the S_xy and make it a new column in the dataframe
+    # Calculate the S_xy
     covariance = []
-    for i in range(len(df)):
-        covariance.append((df.iloc[i, 0] - x_mean) * (df.iloc[i, 1] - y_mean))
+    for i in range(len(dataframe)):
+        covariance.append((dataframe.iloc[i, 0] - x_mean) * (dataframe.iloc[i, 1] - y_mean))
 
     # This calculates the sums for the S_xx, S_yy and S_xy columns
     s_xx_sum = sum(x_variance)
@@ -45,18 +44,42 @@ list_r2_index = ["Linear", "Squared", "Inverse", "Square Root"]
 print('Here is the r squared of various transformations that happen to the data')
 print(f'Linear model {calculate_r_squared(df)}')
 list_r2.append(calculate_r_squared(df))
+
 df = pd.read_csv("Data.csv")
 df[x_column] = df[x_column] ** 2
-print(f'Squared model {calculate_r_squared(df)}')
-list_r2.append(calculate_r_squared(df))
+current_r_squared = calculate_r_squared(df)
+print(f'Squared model {current_r_squared}')
+list_r2.append(current_r_squared)
+
 df = pd.read_csv("Data.csv")
-df[x_column] = 1/df[x_column]
-print(f'Inverse {calculate_r_squared(df)}')
-list_r2.append(calculate_r_squared(df))
+df[x_column] = 1 / df[x_column]
+current_r_squared = calculate_r_squared(df)
+print(f'Inverse {current_r_squared}')
+list_r2.append(current_r_squared)
+
 df = pd.read_csv("Data.csv")
-df[x_column] = df[x_column] ** (1/2)
-print(f'Square root {calculate_r_squared(df)}')
-list_r2.append(calculate_r_squared(df))
+df[x_column] = df[x_column] ** (1 / 2)
+current_r_squared = calculate_r_squared(df)
+print(f'Square root {current_r_squared}')
+list_r2.append(current_r_squared)
+
 # This finds which model provides the lowest r squared value and then prints that out.
 best_fit = np.argmax(list_r2)
 print(list_r2_index[best_fit], "is the best model for fit.")
+
+print("0. Exit Program, 1.Square data, 2. Take the reciprocal (inverse), 3. Square root")
+transform = int(input("How would you like to transform the data "))
+df = pd.read_csv("Data.csv")
+if transform == 0:
+    exit(0)
+elif transform == 1:
+    df[x_column] = df[x_column] ** 2
+elif transform == 2:
+    df[x_column] = 1 / df[x_column]
+elif transform == 3:
+    df[x_column] = df[x_column] ** 0.5
+else:
+    exit(1)
+
+df.to_csv("Data.csv", index=False)
+exit(0)
