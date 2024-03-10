717a5f638326d8fa556ba649a2a94cbaaf00b118
Warner Vance <warnervance@icloud.com>
2024-03-09 21:35:08 -0600

    Optimized LinReg.py to cut time down by 1.2 seconds by adding more pandas stuff

--- LinReg.py
+++ LinReg.py
@@ -11,21 +11,17 @@ x_mean = df.iloc[:, 0].mean()
 y_mean = df.iloc[:, 1].mean()
 # TODO: Update the readme file with more instructions.
 # This calculates the S_xx and S_yy. I
-x_variance = []
-y_variance = []
-for i in range(len(df)):
-    x_variance.append((df.iloc[i, 0] - x_mean)**2)
-for i in range(len(df)):
-    y_variance.append((df.iloc[i, 1] - y_mean)**2)
-
+column_names = df.columns.to_list()
+x_column = column_names[0]
+y_column = column_names[1]
 
 # Calculate the S_xy and make it a new column in the dataframe
-covariance = []
-for i in range(len(df)):
-    covariance.append((df.iloc[i, 0] - x_mean)*(df.iloc[i, 1] - y_mean))
-s_xx_sum = sum(x_variance)
-s_yy_sum = sum(y_variance)
-s_xy_sum = sum(covariance)
+df['S_xx'] = (df[x_column] - x_mean) ** 2
+df['S_yy'] = (df[y_column] - y_mean) ** 2
+df['S_xy'] = (df[x_column] - x_mean) * (df[y_column] - y_mean)
+s_xx_sum = df['S_xx'].sum()
+s_yy_sum = df['S_yy'].sum()
+s_xy_sum = df['S_xy'].sum()
 
 # This finds the r and r squared values
 r = s_xy_sum/(math.sqrt(s_xx_sum)*math.sqrt(s_yy_sum))
@@ -40,21 +36,10 @@ Find the squared error. THat is the squared difference between the actual y valu
 and the y value that would be predicted using the x value, the slope, and the intercept
 '''
 # Calculate the predicted values
-predicted_values = []
-for i in range(len(df)):
-    predicted_values.append(slope*df.iloc[i, 0] + intercept)
-df['Predicted Y'] = predicted_values
-
-squared_error = []
-for i in range(len(df)):
-    squared_error.append((df.iloc[i, 1]-(predicted_values[i])) ** 2)
-df['Squared Error'] = squared_error
-
-df['S_xx'] = x_variance
-df['S_yy'] = y_variance
-df['S_xy'] = covariance
+df['Predicted Y'] = slope*df[x_column] + intercept
+df['Squared Error'] = (df[y_column] - df['Predicted Y']) ** 2
 # This is the sum of the squared error
-SSE = sum(df['Squared Error'])
+SSE = df['Squared Error'].sum()
 
 # This creates a plot with a line of best fit then exports it as graph.png
 graph = sns.regplot(data=df, x=df.iloc[:, 0], y=df.iloc[:, 1], ci=None)
@@ -67,7 +52,7 @@ df.to_csv('Exported_Data.csv', index=False)
 # Export the collected stats summaries as another csv
 output_idx = ('X_Mean', 'Y_Mean', 'R', 'R Squared',
               'Slope', 'Intercept', 'SSE', 'S_xx sum', 'S_yy sum', 'S_xy Sum', "N")
-output_data = (x_mean, y_mean, r, r_squared, slope, intercept, SSE, s_xx_sum, s_yy_sum, s_xy_sum, int(len(df)))
+output_data = (x_mean, y_mean, r, r_squared, slope, intercept, SSE, s_xx_sum, s_yy_sum, s_xy_sum, df.shape[0])
 df_output_data = pd.Series(output_data, index=output_idx)
 df_output_data.to_csv('Exported_Stats.csv')
 # Exits the program
